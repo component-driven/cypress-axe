b3f2515caecbbdf97818582cab8327cc96a455a6
Andrew Van Slaars <avanslaars@gmail.com>
2020-03-25 17:58:10 -0400

    removes task to avoid breaking changes and bumps version

--- index.js
+++ index.js
@@ -11,65 +11,62 @@ Cypress.Commands.add('configureAxe', (configurationOptions = {}) => {
   })
 })
 
-Cypress.Commands.add('checkA11y', (context, options, violationCallback, skipFailures = false) => {
-  cy.window({ log: false })
-    .then(win => {
-      if (isEmptyObjectorNull(context)) context = undefined
-      if (isEmptyObjectorNull(options)) options = undefined
-      if (isEmptyObjectorNull(violationCallback)) violationCallback = undefined
-      return win.axe.run(
-        context ? (context = context) : (context = win.document),
-        options
-      )
-    })
-    .then(({ violations }) => {
-      if (violations.length) {
-        if (violationCallback) violationCallback(violations)
-        cy.wrap(violations, { log: false }).each(v => {
-          const selectors = v.nodes
-            .reduce((acc, node) => acc.concat(node.target), [])
-            .join(', ')
-        
-          Cypress.log({
-            $el: Cypress.$(selectors),
-            name: 'a11y error!',
-            consoleProps: () => v,
-            message: `${v.id} on ${v.nodes.length} Node${
-              v.nodes.length === 1 ? '' : 's'
-            }`
-          })
-        })
-      }
-      return cy.wrap(violations, { log: false })
-    })
-    .then(violations => {
-      if(!skipFailures)  {
-        assert.equal(
-          violations.length,
-          0,
-          `${violations.length} accessibility violation${
-            violations.length === 1 ? '' : 's'
-          } ${violations.length === 1 ? 'was' : 'were'} detected`
+Cypress.Commands.add(
+  'checkA11y',
+  (context, options, violationCallback, skipFailures = false) => {
+    cy.window({ log: false })
+      .then(win => {
+        if (isEmptyObjectorNull(context)) context = undefined
+        if (isEmptyObjectorNull(options)) options = undefined
+        if (isEmptyObjectorNull(violationCallback))
+          violationCallback = undefined
+        return win.axe.run(
+          context ? (context = context) : (context = win.document),
+          options
         )
-      } else {
-          cy.task('log', violations.length === 0 ? "No violations were detected!": `${violations.length} accessibility violation${
-            violations.length === 1 ? '' : 's'
-          } ${violations.length === 1 ? 'was' : 'were'} detected`);
-          let header = "\n\n---------|impact|\t id|\t help|\t helpUrl|---------\n";
-          header = header + "----------------------------------------------------------\n";
-          for(let v = 0 ; v < violations.length ; v++) {
-            if (v == 0) {
-              vDetail = header + `|${violations[v].impact}| ${violations[v].id}| ${violations[v].help}| ${violations[v].helpUrl}|\n`;
-            } else {
-              vDetail = vDetail + `|${violations[v].impact}| ${violations[v].id}| ${violations[v].help}| ${violations[v].helpUrl}|\n`;
-            }
+      })
+      .then(({ violations }) => {
+        if (violations.length) {
+          if (violationCallback) violationCallback(violations)
+          cy.wrap(violations, { log: false }).each(v => {
+            const selectors = v.nodes
+              .reduce((acc, node) => acc.concat(node.target), [])
+              .join(', ')
+
+            Cypress.log({
+              $el: Cypress.$(selectors),
+              name: 'a11y error!',
+              consoleProps: () => v,
+              message: `${v.id} on ${v.nodes.length} Node${
+                v.nodes.length === 1 ? '' : 's'
+              }`
+            })
+          })
+        }
+        return cy.wrap(violations, { log: false })
+      })
+      .then(violations => {
+        if (!skipFailures) {
+          assert.equal(
+            violations.length,
+            0,
+            `${violations.length} accessibility violation${
+              violations.length === 1 ? '' : 's'
+            } ${violations.length === 1 ? 'was' : 'were'} detected`
+          )
+        } else {
+          if (violations.length) {
+            Cypress.log({
+              name: 'a11y violation summary',
+              message: `${violations.length} accessibility violation${
+                violations.length === 1 ? '' : 's'
+              } ${violations.length === 1 ? 'was' : 'were'} detected`
+            })
           }
-          if (violations.length > 0) {
-            cy.task('log',  vDetail);
-          }
-      }
-    })
-})
+        }
+      })
+  }
+)
 
 function isEmptyObjectorNull(value) {
   if (value == null) return true
