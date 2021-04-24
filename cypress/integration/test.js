72f912cae503074cc5db151be7f1a5ce07f82c7e
mcha <>
2021-04-24 11:09:09 +0700

    included scoped check in test

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -1,5 +1,6 @@
 it('works!', () => {
 	cy.visit('/');
 	cy.injectAxe();
+	cy.get('button[data-testid="test-btn"]').checkA11y();
 	cy.checkA11y();
 });
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
